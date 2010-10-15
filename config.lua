9ad854bd6d434c7659db147349cfec315ae62fba
Ygrex <bi@hl.ru>
2010-10-15 21:44:34 +0400

    separate config

---
+++ config.lua
@@ -0,0 +1,24 @@
+--[[
+	shared libraries names
+--]]
+LIBS = {
+	["sqlite3"] = "libsqlite3.so",
+};
+
+--[[
+	database
+--]]
+-- DB name
+DB = "db.sqlite3";
+-- tables to use
+TBL = {
+	["url"] = "url",
+	["prop"] = "prop",
+	["val"] = "val",
+};
+
+--[[
+	GUI
+--]]
+GLADE_FILE = "uuuc.glade";
+
